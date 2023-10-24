9c5f16a4fbd3debdfb30da593ee3c4362b172e55
windwhisper <1309896340@qq.com>
2023-10-24 11:04:40 +0800

    实现了辐射通量计算的demo，但是有问题，计算结果并不准确

--- project_radiant_test/project/demo.cpp
+++ project_radiant_test/project/demo.cpp
@@ -53,17 +53,17 @@ void generateMesh(float width, float height, unsigned int uSize, unsigned int vS
 			if (u != uSize - 1 && v != vSize - 1) {
 				unsigned int* ptr = &index[(u * (vSize - 1) + v) * 6];		// 逆时针绘制矩形面元，法向量朝上
 				ptr[0] = u * vSize + v;							// 左上角
-				ptr[1] = (u + 1) * vSize;						// 左下角
+				ptr[1] = u * vSize + v + 1;					// 左下角	
 				ptr[2] = (u + 1) * vSize + v + 1;			// 右下角
 				ptr[3] = u * vSize + v;							// 左上角
 				ptr[4] = (u + 1) * vSize + v + 1;			// 右下角
-				ptr[5] = u * vSize + v + 1;					// 右上角	
+				ptr[5] = (u + 1) * vSize;						// 右上角
 			}
 		}
 	}
 }
 
-#define SAMPLE_NUM 1000
+#define SAMPLE_NUM 10000
 
 // 用于处理三角形面元的函数，传入三个顶点，以及光源位置，计算这个三角面元的接收辐射量
 // 假设不考虑遮挡，且光源为点光源
@@ -82,16 +82,26 @@ float calculateRadiationByTriangle(const Vertex& a, const Vertex& b, const Verte
 	// 对球面进行均匀采样
 	float dS = 4.0f * PI * powf(light.radius, 2.0f) / SAMPLE_NUM;
 	for (unsigned int i = 0; i < SAMPLE_NUM; i++) {
-		float theta = acosf(1.0f - (float)rand() / RAND_MAX);		// acosf(1 - 2 * (float)rand() / RAND_MAX);	由于取半球，所以theta的范围为[0, pi/2]
+		float theta = acosf(1.0f - (float)rand() / RAND_MAX);		// acosf(1 - 2 * (float)rand() / RAND_MAX);	由于取上半球，所以theta的范围为[0, pi/2]
 		float phi = 2.0f * PI * (float)rand() / RAND_MAX;
+		// // 方案一：以光源中心为原点建立局部坐标，计算出up、right、front三个轴，然后计算出L
+		//glm::vec3 front = glm::normalize(light.position - pos);
+		//glm::vec3 right = glm::normalize(glm::cross(front, glm::vec3(0.0f, 1.0f, 0.0f)));
+		//glm::vec3 up = glm::normalize(glm::cross(right, -front));
+		//glm::vec3 L = glm::normalize(light.radius * sinf(theta) * cosf(phi) * right + light.radius * sinf(theta) * sinf(phi) * up + light.radius * cosf(theta) * L);
+		// // 方案二：计算出front到_up的夹角，计算垂直于front和_up所成平面的旋转轴，将球上半面旋转到front方向，然后计算出L
 		glm::vec3 front = glm::normalize(light.position - pos);
-		glm::vec3 right = glm::normalize(glm::cross(front, glm::vec3(0.0f, 1.0f, 0.0f)));
-		glm::vec3 up = glm::normalize(glm::cross(right, -front));
-		glm::vec3 L = glm::normalize(light.radius * sinf(theta) * cosf(phi) * right + light.radius * sinf(theta) * sinf(phi) * up + light.radius * cosf(theta) * L);
-
+		float angle = acosf(glm::dot(_up, front));
+		glm::vec3 axis = glm::normalize(glm::cross(_up, front));
+		float x = light.radius * sinf(theta) * cosf(phi);
+		float y = light.radius * sinf(theta) * sinf(phi);
+		float z = light.radius * cosf(theta);
+		glm::vec3 samplePos = glm::rotate(glm::vec3(x, y, z), angle, axis) + light.position;
+		glm::vec3 L = glm::normalize(samplePos - pos);
+		float test = glm::dot(N, L);
 		sumbuf += radiance * glm::dot(N, L) * dS;
 	}
-	// 假设三角面元足够小，相对球面光源近似为一点，计算三角面元的面积
+	// 假设三角面元足够小，相对球面光源近似为一点，因此只对该点积分。但该点仍具有微小的面积，因此需要乘以面积
 	float tri_area = glm::length(tmp) / 2.0f;
 	return sumbuf * tri_area;
 }
@@ -103,11 +113,11 @@ int main(int argc, char** argv) {
 	unsigned int uSize = 40, vSize = 40;
 
 	Light light{
-		glm::vec3(0.2f,1.0f,0.2f),
+		glm::vec3(3000.0f,1000.0f * sqrtf(3.0f),0.0f),		// 由于6000远大于10，因此可以近似为点光源，最后辐射通量计算结果应该约等于50
 		glm::vec3(0.0f,0.0f,0.0f),
-		0.0f,
-		1.0f
-	};			// 光源位置，光源方向，光源半径，光照强度。这里只考虑位置和光照强度
+		1.0f,
+		4.0f * PI
+	};			// 光源位置，光源方向，光源半径，光照强度。 这里先不考虑光源方向，并令半径为1，辐射强度为4pi，这样辐射率radiance就为1
 
 
 	generateMesh(10.0f, 10.0f, 40, 40, meshVertex, meshIdx,
@@ -129,10 +139,11 @@ int main(int argc, char** argv) {
 			float rd = calculateRadiationByTriangle(meshVertex[ptr[k * 3 + 0]], meshVertex[ptr[k * 3 + 1]], meshVertex[ptr[k * 3 + 2]], light);		// 将三角形面元三个顶点传入处理函数
 			sumRadiation += rd;
 		}
+		cout << "radiation : " << sumRadiation << endl;
 	}
 
 
-	cout << "break" << endl;
+	cout << "radiation : " << sumRadiation << endl;
 	return 0;
 }
 
--- project_radiant_test/project/proj.h
+++ project_radiant_test/project/proj.h
@@ -17,6 +17,7 @@
 #include "glm/gtx/string_cast.hpp"
 #include "glm/gtc/type_ptr.hpp"
 #include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtx/rotate_vector.hpp"
 
 
 #define WIDTH 1600
