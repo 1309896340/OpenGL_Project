f7a9c2f82dc7442da04b106815ddc6a794e9a2e6
windwhisper <1309896340@qq.com>
2023-11-21 20:21:41 +0800

    将代码封装到类中，但是经过调试出现报错，尚未解决

--- Lsystem_test/Lsystem_test/LSystem.hpp
+++ Lsystem_test/Lsystem_test/LSystem.hpp
@@ -2,8 +2,10 @@
 #include <iostream>
 #include <string>
 #include <map>
-
+#include <regex>
 #include <cctype>
+
+#include "utils.h"
 
 using namespace std;
 typedef map<string, string> LRule;
@@ -75,12 +77,215 @@ public:
 };
 
 class PD0LSystem :public LSystem {		// 带参数的D0L系统
+private:
 public:
 	PD0LSystem(const string& axiom, LRule& mapper) :LSystem(axiom, mapper) {}
 	virtual string iterate(const string& oldStr) {
-		string newStr;
-		
-		return newStr;
+		stringstream newStr;
+
+		// 输入axiom和mapper
+		smatch m;
+		for (auto pos = oldStr.cbegin(), end = oldStr.cend(); regex_search(pos, end, m, expressionExp); pos = m.suffix().first) {
+			string elem = m.str(0);
+			// 按顺序取出所有参数
+			vector<float> params;
+			string paramStr = m.str(2);
+			size_t startPos = 0;
+			for (size_t endPos = paramStr.find(',', startPos); endPos != string::npos; startPos = endPos + 1, endPos = paramStr.find(',', startPos)) {
+				params.push_back((float)atof(paramStr.substr(startPos, endPos - startPos).c_str()));
+			}
+			params.push_back((float)atof(paramStr.substr(startPos).c_str()));
+			// 在mapper中寻找匹配的规则
+			auto mapperElemIter = find_if(mapper.begin(), mapper.end(), [&m](pair<string, string> keyStr) {
+				return keyStr.first.substr(0, 1) == m.str(1);
+				});
+			if (mapperElemIter == mapper.end())	// 没有找到匹配列表，跳过
+				continue;
+			// 取出映射到的目标表达式，根据其中的计算规则，计算出结果，并将结果重组到原表达式中
+			string srcFormat = mapperElemIter->first;				// "A(x, y)"
+			string dstFormat = mapperElemIter->second;		// "B(0.2*y, 0.5*x)"
+			// 提取srcFormat中的两个字符x和y，动态生成一用于替换的正则表达式
+			vector<string> paramsName;
+			// 假定paramName只有1个字符，在第0位置，且第1个字符为左括号，则括号内开始字符为第2，结束字符为srcFormat.length() - 1
+			string subSrcFormat = srcFormat.substr(2, srcFormat.length() - 3);
+			startPos = 0;
+			for (size_t endPos = subSrcFormat.find(',', startPos); endPos != string::npos; startPos = endPos + 1, endPos = subSrcFormat.find(',', startPos)) {
+				string pname = subSrcFormat.substr(startPos, endPos - startPos);
+				strip(pname);
+				paramsName.push_back(pname.substr(0, 1));
+			}
+			string pname = subSrcFormat.substr(startPos);
+			strip(pname);
+			paramsName.push_back(pname.substr(0, 1));
+			// 现在paramsName中存放了所有参数的名称（例如这里是"x"和"y"），构造一个map
+			map<string, float> paramsMap;
+			for (unsigned int i = 0; i < params.size(); i++)
+				paramsMap[paramsName[i]] = params[i];
+			// 遍历dstFormat并替换其中的参数
+			stringstream replacedStringStream;
+			for (unsigned int i = 0; i < dstFormat.length(); i++) {
+				string src = dstFormat.substr(i, 1);
+				if (paramsMap.count(src) <= 0) {
+					replacedStringStream << src;
+				}
+				else {
+					replacedStringStream << to_string(paramsMap[src]);
+				}
+			}
+			//cout << "文本替换后：" << replacedStringStream.str() << endl;
+			// 将替换完成后的字符串中的表达式计算出来，假定这里只使用不带括号的加减乘除
+			string stringNotCompute = replacedStringStream.str();
+			// stringNotCompute是替换后的表达式，可能具有多个带参符号，需要再进行一轮遍历
+			smatch mm;
+			stringstream dstString;		// 用来存储最终的表达式
+			string compStr;
+			for (auto spos = stringNotCompute.cbegin(), send = stringNotCompute.cend(); regex_search(spos, send, mm, expressionExp); spos = mm.suffix().first) {
+				dstString << mm.str(1) << "(";
+				// 取出逗号分割的每个表达式并进行计算
+				startPos = 0;
+				string tmpStr = mm.str(2);
+				for (size_t endPos = tmpStr.find(',', startPos); endPos != string::npos; startPos = endPos + 1, endPos = tmpStr.find(',', startPos)) {
+					compStr = tmpStr.substr(startPos, endPos - startPos);
+					dstString << to_string(expressionEvaluate(compStr)) << ",";
+				}
+				compStr = tmpStr.substr(startPos);
+				dstString << to_string(expressionEvaluate(compStr)) << ")";
+			}
+			newStr << dstString.str();
+			//cout << "最终表达式：" << dstString.str() << endl;
+			//cout << "===========================================" << endl;
+		}
+		return newStr.str();
+	}
+	
+
+	static inline void strip(string& s) {
+		if (!s.empty()) {
+			s.erase(0, s.find_first_not_of(" "));
+			s.erase(s.find_last_not_of(" ") + 1);
+		}
+	}
+	// 中缀表达式转换为后缀表达式，newExpr 为后缀表达式，elemType为每个元素的类型，0为数字，1为运算符
+	static inline void expression2RPN(const string& expression, vector<string>& newExpr, vector<ElemType>& elemType) {
+		vector<char> operators;		// 运算符栈
+		newExpr.clear();
+		elemType.clear();
+		for (unsigned int i = 0; i < expression.size(); i++) {
+			char c = expression[i];
+			if (c == ' ')		// 跳过空格
+				continue;
+			if (isNumber(c)) {
+				unsigned int startPos = i;
+				while (isNumber(expression[++i]));
+				newExpr.push_back(expression.substr(startPos, i - startPos));
+				elemType.push_back(ElemType::NUMBER);
+				// 数字串压栈，i当前位置更新为扫描到的下一个非数字字符，也就是运算符
+				i--;	// 由于for循环结尾会自增，所以这里先减1
+			}
+			else if (c == '(') {
+				//newExpr.push_back(expression.substr(i, 1));
+				operators.push_back(c);
+			}
+			else if (c == ')') {
+				char top = operators.back();
+				while (top != '(') {
+					newExpr.push_back(string(1, top));
+					elemType.push_back(ElemType::OPERATOR);
+					operators.pop_back();
+					top = operators.back();
+				}
+				operators.pop_back();	// 取出左括号，但不压入newExpr
+			}
+			else if (isOperator(c)) {
+				// 处理负号歧义问题
+				// c是第0个元素，或者c前面是左括号，或者c前面是运算符，那么c就是负号。否则是普通减号
+				if (c == '-' && (i == 0 || expression[i - 1] == '(' || isOperator(expression[i - 1]))) {
+					// 如果是单目负号，将其处理替换为'|'以区分
+					c = '|';
+				}
+				// =========================
+				if (operators.empty())
+					operators.push_back(c);
+				else {
+					char top = operators.back();
+					if (top == '(')
+						operators.push_back(c);
+					else {
+						// 比较优先级
+						char compRes = priorComparaLookupTable[opr2id(c)][opr2id(top)];
+						if (compRes <= 1) {
+							// 当前优先级低于栈顶运算符优先级
+							newExpr.push_back(string(1, top));
+							elemType.push_back(ElemType::OPERATOR);
+							operators.pop_back();
+							operators.push_back(c);
+						}
+						else if (compRes == 2) {
+							operators.push_back(c);
+						}
+					}
+				}
+			}
+		}
+		// 将operators中剩余的运算符压入newExpr
+		while (!operators.empty()) {
+			char top = operators.back();
+			newExpr.push_back(string(1, top));
+			elemType.push_back(ElemType::OPERATOR);
+			operators.pop_back();
+		}
+		//cout << "原始表达式：" << endl << expression << endl;
+		//cout << "生成的后缀表达式：" << endl << newExpr << endl;
+	}
+	// 对后缀表达式求值
+	static inline float evalRPN(vector<string>& rpn, vector<ElemType>& elemType) {
+		vector<float> stack;
+		for (unsigned int i = 0; i < rpn.size(); i++) {
+			ElemType& type = elemType[i];
+			if (type == ElemType::NUMBER) {
+				stack.push_back((float)atof(rpn[i].c_str()));
+			}
+			else if (type == ElemType::OPERATOR) {
+				float params[2];
+				char opr = rpn[i][0];
+				float res;
+				for (unsigned int k = 0; k < (unsigned int)operatorNumLookupTable[opr2id(opr)]; k++) {
+					params[k] = stack.back();
+					stack.pop_back();
+				}
+				switch (opr) {
+				case '+':
+					res = params[1] + params[0];
+					break;
+				case '-':
+					res = params[1] - params[0];
+					break;
+				case '*':
+					res = params[1] * params[0];
+					break;
+				case '/':
+					res = params[1] / params[0];
+					break;
+				case '|':
+					res = -params[0];
+					break;
+				default:
+					assert(0);
+					break;
+				}
+				stack.push_back(res);
+			}
+		}
+		return stack[0];
+	}
+	// 转换为后缀表达式并求值
+	static inline float expressionEvaluate(const string& expression) {
+		vector<string> RPNExpr;
+		vector<ElemType> elemType;
+		expression2RPN(expression, RPNExpr, elemType);
+		float res = evalRPN(RPNExpr, elemType);
+		//cout << "表达式 \"" << expression << "\" 的结果为：" << res << endl;	// 调试信息
+		return res;
 	}
 };
 
